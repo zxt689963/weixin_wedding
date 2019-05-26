323a1ffd9290ada42a8a8e6c44e40ba3383a4646
anfly <zyafnly1832@163.com>
2019-05-26 09:33:09 +0800

    '完成'

--- components/appointment/index.js
+++ components/appointment/index.js
@@ -1,26 +1,66 @@
 import {
-    AppointmentModel
+  AppointmentModel
 } from '../../models/appointment'
 
 const appointmentModel = new AppointmentModel()
 
 Component({
-    properties: {
-    },
+  properties: {
+  },
+  data: {
+    // 可通过接口预设时间范围
+    endDate:'2022-01-01',
+    startDate:'2018-01-01',
+    endTime:'18:00',
+    startTime:'09:00',
+    date: '',
+    year:'',
+    month:'',
+    day:'',
+    time:'',
+    hour:'',
+    minute:''
+  },
 
-    data: {
+  methods: {
+    formSubmit: function (e) {
+      console.log(e.detail.behavior)
+      appointmentModel.createAppointment(e.detail.value.name, e.detail.value.contact, e.detail.value.content)
+        .then(res => {
+          wx.showToast({
+            title: '提交成功',
+            icon: "none"
+          })
+        })
     },
-
-    methods: {
-        formSubmit: function (e) {
-            console.log(e.detail.behavior)
-            appointmentModel.createAppointment(e.detail.value.name, e.detail.value.contact, e.detail.value.content)
-                .then(res => {
-                    wx.showToast({
-                        title: '提交成功',
-                        icon: "none"
-                    })
-                })
-        },
+    bindDateChange: function (e) {
+      var a = e.detail.value.split('-');
+      this.setData({
+        date:e.detail.value,
+        year:a[0],
+        month:a[1],
+        day:a[2]
+      })
+    },
+    bindTimeChange:function(e){
+      var t = e.detail.value.split(':')
+      this.setData({
+        time:e.detail.value,
+        hour:t[0],
+        minute:t[1]
+      })
     }
+  },
+  attached(){
+    var time = new Date();
+    var year = time.getFullYear();
+    var month = time.getMonth()+1;
+    var day = time.getDate();
+    var hour = time.getHours();
+    var minute = time.getMinutes();
+    this.setData({
+      date:year+'-'+month+'-'+day,
+      time:hour+':'+minute,
+    })
+  }
 })
