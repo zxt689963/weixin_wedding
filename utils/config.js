c08fafe803875d5de55597eef102e415cd83e153
anfly <zyanfly1832@163.com>
2019-05-27 17:56:35 +0800

    修改预约字体大小

--- utils/config.js
+++ utils/config.js
@@ -1,8 +1,8 @@
 const config = {
-    //api_base_url: 'https://zhengjiajun.com/api/v1/',
+    api_base_url: 'https://zhengjiajun.com/api/v1/',
     //appkey: "0nmjjyw9lt"
-    api_base_url: 'http://localhost:3000/api/v1/',
-    appkey: "yw8wtk893m",
+    // api_base_url: 'http://localhost:3000/api/v1/',
+    appkey: "0nmjjyw9lt",
 }
 
 export { config }
