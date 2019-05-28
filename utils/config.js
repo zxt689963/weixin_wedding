2c5e3b32da0fb75094282a37ccbf223b57f39360
jayzen <jayzen@foxmail.com>
2019-05-28 18:38:04 +0800

    do some fix

--- utils/config.js
+++ utils/config.js
@@ -1,8 +1,9 @@
 const config = {
-    // api_base_url: 'https://zhengjiajun.com/api/v1/',
-    // appkey: "t9f726cgrp"
-    api_base_url: 'http://localhost:3000/api/v1/',
-    appkey: "29pb86wrg0"
+    api_base_url: 'https://zhengjiajun.com/api/v1/',
+    appkey: "t9f726cgrp"
+    // api_base_url: 'http://localhost:3000/api/v1/',
+    // appkey: "29pb86wrg0"
 }
 
 export { config }
+
