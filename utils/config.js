ec209b2c55b0cc817e1798b13222251ab05ab512
jayzen <jayzen@foxmail.com>
2019-05-29 20:21:02 +0800

    do some fix

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 const config = {
-    api_base_url: 'https://zhengjiajun.com/api/v1/',
+    api_base_url: 'https://eco-news.cn/api/v1/',
     appkey: "t9f726cgrp"
     // api_base_url: 'http://localhost:3000/api/v1/',
     // appkey: "29pb86wrg0"
